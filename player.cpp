0d1d8a55cca45a617695d7d5c752537b4398d146
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-12-17 19:25:07 -0800

    Create player.cpp
    Defines what should happen when the arrow keys and spacebar are pressed. some debugging features. Spawn function to be defined later when I add enmies

---
+++ player.cpp
@@ -0,0 +1,42 @@
+#include "player.h"
+#include <QKeyEvent>
+#include <QGraphicsRectItem>
+#include <QGraphicsScene>
+#include <QDebug>
+
+Player::Player()
+{
+    setRect(0,0,100,100);
+}
+
+void Player::keyPressEvent(QKeyEvent *event)
+{
+    qDebug() << "Player knows you pressed a key.";
+    //qDebug() << "New position is (" << pos().x() << "," << pos().y() << ")";
+    if(event->key() == Qt::Key_Left){
+        if(pos().x() > 0)
+            setPos(x()-5,y());
+    }
+    else if(event->key() == Qt::Key_Right){
+        if(pos().x()+100 < 800)
+            setPos(x()+5,y());
+    }
+    else if(event->key() == Qt::Key_Up){
+        if(pos().y() > 0)
+            setPos(x(),y()-5);
+    }
+    else if(event->key() == Qt::Key_Down){
+        if(pos().y() + 100 < 800)
+            setPos(x(),y()+5);
+    }
+    else if (event->key() == Qt::Key_Space){
+        Bullet* bullet = new Bullet();
+        scene()->addItem(bullet);
+        bullet->setPos(x() + rect().width()/2 - bullet->rect().width()/2, y());
+    }
+}
+
+void Player::spawn()
+{
+    
+}
