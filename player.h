f27806645fa7e4a35f8dc6c12a0d615e339c1230
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-12-17 19:23:15 -0800

    Create player.h
    My player class which determines the appearance, movement and shooting ability of the player. Also spawns enemies

---
+++ player.h
@@ -0,0 +1,17 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <QObject>
+#include <QGraphicsRectItem>
+
+class Player: public QObject, public QGraphicsRectItem{
+    Q_OBJECT
+public:
+    //constructs player of size 100 by 100 pixels
+    Player();
+public slots:  
+    //establishes movement and shooting abilities of the player
+    void keyPressEvent(QKeyEvent* event);
+    void spawn();
+};
+
+#endif PLAYER_H
