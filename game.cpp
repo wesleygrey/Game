a5a4ee73f49cb429c5e9c84c98b3aab7488d4775
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-12-17 19:20:41 -0800

    Create game.cpp
    Creates a player and places it in a scene. so far just a rectangle in an 800x800 pixel view

---
+++ game.cpp
@@ -0,0 +1,28 @@
+#include "Game.h"
+#include <QTimer>
+
+Game::Game(QWidget *parent){
+    //create a player
+    Player* player = new Player();
+
+    //create a scene and add player
+    QGraphicsScene* myScene = new QGraphicsScene();
+    myScene->addItem(player);
+
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    //set sizes of view and screen
+    //set view size first so scene can reference view's coordinates for its first 2 parameters
+    setFixedSize(800,800);
+    myScene->setSceneRect(0,0,800,800);
+
+    //set the position of the player in the lower middle of the view/scene
+    player->setPos(width()/2 - player->rect().width()/2,
+                   height() - 2*player->rect().height());
+
+
+    //make the player focusable and focus on it
+    player->setFlag(QGraphicsItem::ItemIsFocusable);
+    player->setFocus();
+    }
