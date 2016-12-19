968103e6fc37c22a024464a66f2a7fab8153bf56
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-12-19 15:03:51 -0800

    Create choosers.cpp
    Defines the what should happen when a button is pressed and hovered over

---
+++ choosers.cpp
@@ -0,0 +1,41 @@
+#include "choosers.h"
+#include <QGraphicsTextItem>
+#include <QBrush>
+
+Button::Button(QString *name, QGraphicsRectItem *parent): QGraphicsRectItem(parent){
+    setRect(0,0, 500, 400);
+    QBrush bColor;
+    bColor.setStyle(Qt::SolidPattern);
+    bColor.setColor(Qt::darkRed);
+    setBrush(bColor);
+
+    QGraphicsTextItem* bText = new QGraphicsTextItem(name, this);
+    int txPos = rect().width()/2 - text->boundingRect().width()/2;
+    int tyPos = rect().height()/2 - text->boundingRect().height()/2;
+    bText->setPos(txPos, tyPos);
+
+    setAcceptHoverEvents(true);
+}
+
+
+void Button::mousePressEvent(QGraphicsSceneMouseEvent *press)
+{
+  emit clicked();
+}
+
+
+void Button::hoverEnterEvent(QGraphicsSceneHoverEvent *in)
+{
+    QBrush bColor;
+    bColor.setStyle(Qt::SolidPattern);
+    bColor.setColor(Qt::darkCyan);
+    setBrush(bColor);
+}
+
+void Button::hoverLeaveEvent(QGraphicsSceneHoverEvent *out)
+{
+    QBrush bColor;
+    bColor.setStyle(Qt::SolidPattern);
+    bColor.setColor(Qt::darkRed);
+    setBrush(bColor);
+}
