203f732a314bc233900370e5d39a9e9d86c45363
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-12-19 21:25:16 -0800

    Instantiate a game obj
    right now only Game::start() is funtioning properly. When I call Game::displayMainMenu() the program crashes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <QApplication>
+#include "game.h"
+
+Game* myGame;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    myGame = new Game();
+    myGame->show();
+    //myGame->displayMainMenu(); <--breaks...
+    myGame->launchGame();
+
     return a.exec();
 }
