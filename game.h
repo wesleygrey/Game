676703b11fb14c873d74eb2a17aff334dc545e44
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-12-19 21:26:25 -0800

    Added slots and int level

--- game.h
+++ game.h
@@ -5,13 +5,24 @@
 #include <QGraphicsView>
 #include <QWidget>
 #include "player.h"
+#include "gametext.h"
 
 class Game: public QGraphicsView{
 public:
-    Game(QWidget* parent=0);
-    QGraphicsScene* scene;
+    Game(QWidget* parent=NULL);
+    QGraphicsScene* myScene;
     Player* player;
-    
+    Score* score;
+    Health* health;
+public slots:
+    void displayMainMenu();
+    void launchGame();
+    void level_up();
+private:
+    int level = 0;
+
+
 };
 
 #endif // GAME_H
+
