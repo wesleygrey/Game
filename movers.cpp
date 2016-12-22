d4ba3e727d29c1c6bed02f329c4c33dc7a9de355
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-12-21 21:41:19 -0800

    Implemented the Coin class

--- movers.cpp
+++ movers.cpp
@@ -61,3 +61,34 @@ void Enemy::move()
     }
     setPos(x(), y()+5);
 }
+
+Coin::Coin(){
+    setRect(0,0,20,20);
+    //randomly spawn on the left of the screen
+    //no more than 400 pixels above bottom of the screen
+    int rand_num = rand() % 680;
+    setPos(0, rand_num);
+    //connect a timer to the move() member function
+    QTimer* cTimer = new QTimer;
+    connect(cTimer, SIGNAL(timeout()), this, SLOT(move()));
+    cTimer->start(75);
+}
+
+void Coin::move()
+{
+    //if coin leaves view deduct health, delete coin
+    if(pos().x() > myGame->sceneWidth){
+        delete this;
+        return;
+    }
+    //or if coin collides with player
+    QList<QGraphicsItem*> collision = collidingItems();
+    for(size_t pos = 0; pos != collision.size(); ++pos){
+        if(typeid(*(collision[pos])) == typeid(Player)){
+            delete this;
+            myGame->money->update();
+            return;
+        }
+    }
+    setPos(x()+ 10, y());
+}
