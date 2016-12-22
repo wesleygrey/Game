8756db34608f6b7bf3662fd6873057a3c516a806
Wesley Hartmann <wesleygrey@g.ucla.edu>
2016-12-21 21:29:13 -0800

    Added Money variable and showRules() function

--- game.h
+++ game.h
@@ -14,14 +14,17 @@ public:
     Player* player;
     Score* score;
     Health* health;
+    Money* money;
+    //this will be the height and width of the scene in pixels
+    const int sceneWidth = 800;
+    const int sceneHeight = 700;
 public slots:
     void displayMainMenu();
     void launchGame();
+    void showRules();
     void level_up();
 private:
     int level = 0;
-
-
 };
 
 #endif // GAME_H
