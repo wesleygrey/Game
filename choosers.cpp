cf2c87e5bcea687d37c3b1f4d96f9907d79aaf22
Wesley Hartmann <wesleygrey@g.ucla.edu>
2017-03-23 19:06:58 -0700

    final touches

--- choosers.cpp
+++ choosers.cpp
@@ -1,18 +1,24 @@
 #include "choosers.h"
 #include <QGraphicsTextItem>
 #include <QBrush>
+#include <QDebug>
 
-Button::Button(QString *name, QGraphicsRectItem *parent): QGraphicsRectItem(parent){
-    setRect(0,0, 500, 400);
+Button::Button(QString name, QGraphicsItem *parent): QGraphicsRectItem(parent){
+
+    setRect(0,0, 100, 50);
+    //qDebug() << "rect set";
     QBrush bColor;
     bColor.setStyle(Qt::SolidPattern);
-    bColor.setColor(Qt::darkRed);
+    bColor.setColor(Qt::darkCyan);
     setBrush(bColor);
+    qDebug() << "color set";
 
     QGraphicsTextItem* bText = new QGraphicsTextItem(name, this);
+    qDebug() << "name created";
     int txPos = rect().width()/2 - bText->boundingRect().width()/2;
     int tyPos = rect().height()/2 - bText->boundingRect().height()/2;
     bText->setPos(txPos, tyPos);
+    qDebug() << "name set";
 
     setAcceptHoverEvents(true);
 }
@@ -28,7 +34,7 @@ void Button::hoverEnterEvent(QGraphicsSceneHoverEvent *in)
 {
     QBrush bColor;
     bColor.setStyle(Qt::SolidPattern);
-    bColor.setColor(Qt::darkCyan);
+    bColor.setColor(Qt::darkYellow);
     setBrush(bColor);
 }
 
@@ -36,6 +42,6 @@ void Button::hoverLeaveEvent(QGraphicsSceneHoverEvent *out)
 {
     QBrush bColor;
     bColor.setStyle(Qt::SolidPattern);
-    bColor.setColor(Qt::darkRed);
+    bColor.setColor(Qt::darkCyan);
     setBrush(bColor);
 }
